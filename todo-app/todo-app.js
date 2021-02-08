bb1fb7dea1a7daeb520d07ccbff7d2c0375c8f2b
max-b10 <bungaymax@gmail.com>
2021-02-08 12:57:05 +0000

    Refactoring with new functions.js file.

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,74 +1,22 @@
-const todos = [
-  {
-    text: "Order cat food",
-    completed: true,
-  },
-  {
-    text: "Clean Kitchen",
-    completed: false,
-  },
-  {
-    text: "Exercise",
-    completed: true,
-  },
-  {
-    text: "Do some work",
-    completed: false,
-  },
-  {
-    text: "Got to bed",
-    completed: true,
-  },
-];
+const todos = getSavedTodos();
 
 // A filters object acting as one of the parameters in the renderToDos function.
 const filters = {
   searchText: "",
   hideCompleted: false,
 };
-const renderToDos = function (todos, filters) {
-  let filteredToDos = todos.filter(function (todo) {
-    return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
-  });
-  // Logic to hide the completed ToDos when the checkbox is ticked.
-  filteredToDos = filteredToDos.filter(function (todo) {
-    if (filters.hideCompleted) {
-      return !todo.completed;
-    } else {
-      return true;
-    }
-  });
 
-  // Print message saying how many toDos are left.
-  const incompleteToDos = filteredToDos.filter(function (todo) {
-    return !todo.completed;
-  });
-  // Using innerHTML wipes out the div of any previous html before adding the filtered notes.
-
-  document.querySelector("#todos").innerHTML = "";
-
-  const summary = document.createElement("h2");
-  summary.textContent = `You have ${incompleteToDos.length} To Dos left`;
-  document.querySelector("#todos").appendChild(summary);
-
-  // Add a paragragh for each ToDo in the array.
-  filteredToDos.forEach(function (todo) {
-    const addPara = document.createElement("p");
-    addPara.textContent = todo.text;
-    document.querySelector("#todos").appendChild(addPara);
-  });
-};
-renderToDos(todos, filters);
-// Search Text filter, which calls renderToDos after each user input.
+renderTodos(todos, filters);
+// Search Text filter, which calls renderTodos after each user input.
 document.querySelector("#search-text").addEventListener("input", function (e) {
   filters.searchText = e.target.value;
-  renderToDos(todos, filters);
+  renderTodos(todos, filters);
 });
 
 // Stopping default behaviour(submission) of the submit button field.
-// Then console logging the createToDo value.
+// Then pushing on the createToDo value.
 document
-  .querySelector("#createToDoForm")
+  .querySelector("#createTodoForm")
   .addEventListener("submit", function (e) {
     e.preventDefault();
     // Push a new object to the todos array:
@@ -76,8 +24,9 @@ document
       text: e.target.elements.text.value,
       completed: false,
     });
+    saveTodos(todos);
     //Rerender the list of todos:
-    renderToDos(todos, filters);
+    renderTodos(todos, filters);
     e.target.elements.text.value = "";
   });
 
@@ -86,5 +35,5 @@ document
   .querySelector("#hideCompleted")
   .addEventListener("change", function (e) {
     filters.hideCompleted = e.target.checked;
-    renderToDos(todos, filters);
+    renderTodos(todos, filters);
   });
