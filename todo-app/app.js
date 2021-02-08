0606707fa2e80f0c09e9da4556e6618f0e8a7f9a
max-b10 <bungaymax@gmail.com>
2021-02-08 09:14:58 +0000

    Added a checkbox filter.

--- todo-app/app.js
+++ todo-app/app.js
@@ -21,13 +21,24 @@ const todos = [
   },
 ];
 
+// A filters object acting as one of the parameters in the renderToDos function.
 const filters = {
   searchText: "",
+  hideCompleted: false,
 };
 const renderToDos = function (todos, filters) {
-  const filteredToDos = todos.filter(function (todo) {
+  let filteredToDos = todos.filter(function (todo) {
     return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
   });
+  // Logic to hide the completed ToDos when the checkbox is ticked.
+  filteredToDos = filteredToDos.filter(function (todo) {
+    if (filters.hideCompleted) {
+      return !todo.completed;
+    } else {
+      return true;
+    }
+  });
+
   // Print message saying how many toDos are left.
   const incompleteToDos = filteredToDos.filter(function (todo) {
     return !todo.completed;
@@ -41,14 +52,14 @@ const renderToDos = function (todos, filters) {
   document.querySelector("#todos").appendChild(summary);
 
   // Add a paragragh for each ToDo in the array.
-  filteredToDos.forEach(function (object) {
+  filteredToDos.forEach(function (todo) {
     const addPara = document.createElement("p");
-    addPara.textContent = object.text;
+    addPara.textContent = todo.text;
     document.querySelector("#todos").appendChild(addPara);
   });
 };
 renderToDos(todos, filters);
-
+// Search Text filter, which calls renderToDos after each user input.
 document.querySelector("#search-text").addEventListener("input", function (e) {
   filters.searchText = e.target.value;
   renderToDos(todos, filters);
@@ -60,10 +71,20 @@ document
   .querySelector("#createToDoForm")
   .addEventListener("submit", function (e) {
     e.preventDefault();
+    // Push a new object to the todos array:
     todos.push({
       text: e.target.elements.text.value,
       completed: false,
     });
+    //Rerender the list of todos:
     renderToDos(todos, filters);
     e.target.elements.text.value = "";
+  });
+
+// Event Listener for hideCompleted checkbox.
+document
+  .querySelector("#hideCompleted")
+  .addEventListener("change", function (e) {
+    filters.hideCompleted = e.target.checked;
+    renderToDos(todos, filters);
   });
